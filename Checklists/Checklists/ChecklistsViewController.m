4a8ccaa20af39cfd725305efc93f570c6afa0785
Ming-Zhe <law.gravitys@gmail.com>
2014-02-11 21:15:05 +0800

    new

--- Checklists/Checklists/ChecklistsViewController.m
+++ Checklists/Checklists/ChecklistsViewController.m
@@ -27,10 +27,23 @@
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 1;
+    return 5;
 }
--(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+-(UITableViewCell*)tableView: (UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    UILabel *label = (UILabel*)[cell viewWithTag:1000];
+    if(indexPath.row ==0){
+        label.text = @"观看嫦娥⻜飞天和⽟玉兔升空的视频";
+    }else if(indexPath.row ==1){
+        label.text = @"了解Sony a7和MBP的最新价格";
+    }else if(indexPath.row ==2){
+        label.text = @"复习苍⽼老师的经典视频教程";
+    }else if(indexPath.row ==3){
+        label.text = @"去电影院看地⼼心引⼒力";
+    }else if(indexPath.row ==4){
+        label.text = @"看⻄西甲巴萨新败的⽐比赛回放";
+    }
     return cell;
 }
 
