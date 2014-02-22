8fdf72b029acb990d7e5d5eda740fc214ee44b2d
Ming-Zhe <law.gravitys@gmail.com>
2014-02-22 17:48:48 +0800

    new

---
+++ BST/BST/main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  BST
+//
+//  Created by Ming-Zhe on 14-2-21.
+//  Copyright (c) 2014年 Ming-Zhe. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+typedef struct bst_node{
+    int key;
+    struct Node* lchild;
+    struct Node* rchild;
+}bst_node;
+
+
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
