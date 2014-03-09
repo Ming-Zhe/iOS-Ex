862e15fde1d2a0334253add8abf57377566d0a24
Ming-Zhe <law.gravitys@gmail.com>
2014-03-09 15:49:17 +0800

    new

---
+++ Trie/Trie/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Trie
+//
+//  Created by Ming-Zhe on 14-3-9.
+//  Copyright (c) 2014年 Ming-Zhe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
